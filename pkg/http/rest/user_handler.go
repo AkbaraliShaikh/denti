adbf835e86ac939a7e2bfefed4403ce637b8bbe2
Akbar Shaikh <aashaikh55@gmail.com>
2022-12-01 17:49:35 +0530

    feat: organized the imports

--- pkg/http/rest/user_handler.go
+++ pkg/http/rest/user_handler.go
@@ -1,9 +1,10 @@
 package rest
 
 import (
+	"net/http"
+
 	"github.com/akbaralishaikh/denti/pkg/logger"
 	"github.com/akbaralishaikh/denti/pkg/user"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
